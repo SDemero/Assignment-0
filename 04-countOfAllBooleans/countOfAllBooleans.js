4f6cff232bf1ff5ca25c754cc70faca883d00b4b
SDemero <61198011+SDemero@users.noreply.github.com>
2021-01-03 18:36:46 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,13 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  var x ;
+	var counter = 0;
+	for(x = 0; x < arr.length; x++){
+    
+		if (typeof arr[x] === "boolean"){
+			counter++;
+    }
+  }return counter;
+      
 }
 
 // Do not edit this line;
