48bd044dc99433133abdf966968e58c9997864ec
SDemero <61198011+SDemero@users.noreply.github.com>
2021-01-03 18:39:55 -0500

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,15 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+ var x;
+ var counter = 0;
+	for(x = 0; x < arr.length; x++){
+    
+		if(typeof arr[x] === "boolean" || typeof arr[x] === "string"){
+			counter++;
+		}
+	}
+	return counter;
 }
+
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
