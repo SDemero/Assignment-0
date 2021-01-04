a5ff47c824c95d981de4cf1468fbc96b22074a43
SDemero <61198011+SDemero@users.noreply.github.com>
2021-01-03 23:35:14 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,13 @@
 function pairSum(nums, target) {
-  // Insert code here;
-}
+  if (nums.length <= 1) {
+    throw 'Size of array is less than two';
+ }
+  for (let x = 0; x < nums.length - 1; x++) {
+  	for (let y = x + 1; y< nums.length; y++) {
+  	  if (nums[y] + nums[x] == target) {
+  	    return true;
+  	  } } }
+  return false; }
 
 // Do not edit this line;
 module.exports = pairSum;
