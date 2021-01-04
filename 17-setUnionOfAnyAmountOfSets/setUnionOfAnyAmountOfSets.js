b379f73372123a8249a75a5d985bcff5a5f03a4f
SDemero <61198011+SDemero@users.noreply.github.com>
2021-01-03 23:31:20 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,10 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let result = new Set();
+	for (var set of args) {
+		for (var value of set) {
+			result.add(value);
+		}}
+	return result;
 }
 
 // Do not edit this line;
