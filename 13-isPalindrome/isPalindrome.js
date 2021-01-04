ef517cc31459ac496d4948c89f5060ac8d1985ed
SDemero <61198011+SDemero@users.noreply.github.com>
2021-01-03 22:10:52 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Insert code here;
+ let length = word.length;
+for (let i = 0; i < length/2; i++) {
+    if (word[i] !== word[length-1-i]) {
+      return false;
+    }}
+  return true;
 }
 
 // Do not edit this line;
